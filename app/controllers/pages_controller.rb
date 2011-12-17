391820f16e4b87e0b3cc61cab1d189c2fefcda22
Marco Ndoping <mndoping@hawk.iit.edu>
2011-12-16 20:32:08 -0600

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
+
   def home
+    @title = "Home"
+  end
+
+  def about
+    @title = "Contact"
   end
 
   def contact
+    @titel = "About"
   end
 
 end
